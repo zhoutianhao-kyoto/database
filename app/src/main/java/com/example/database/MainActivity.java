66412c7aa8ff4cd5406373aa5a4e2c489f457678
kcg-yyu <y_yu@kcg.ac.jp>
2019-11-20 14:37:49 +0900

    Add file read/write part

--- app/src/main/java/com/example/database/MainActivity.java
+++ app/src/main/java/com/example/database/MainActivity.java
@@ -5,20 +5,32 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText textmsg;
+    TextView mainView;
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textmsg = (EditText)findViewById(R.id.editText);
+        mainView = (TextView) findViewById(R.id.mainView);
 
         SQLiteOpenHelper dbHelper = new DatabaseHelper(getApplicationContext());
-
-        TextView mainView = (TextView) findViewById(R.id.mainView);
 
         try{
             SQLiteDatabase db = dbHelper.getReadableDatabase();
@@ -32,6 +44,39 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch(SQLiteException e) {
             mainView.setText("SQL error happened:\n" + e.toString());
+        }
+    }
+
+    public void WriteBtn(View v) {
+        // add-write text into file
+        try {
+            FileOutputStream fileout = openFileOutput("mytextfile.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
+            outputWriter.write(textmsg.getText().toString());
+            outputWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ReadBtn(View v) {
+        //reading text from file
+        try {
+            FileInputStream fileIn = openFileInput("mytextfile.txt");
+            InputStreamReader InputRead = new InputStreamReader(fileIn);
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            String s=""; int charRead;
+
+            while ((charRead = InputRead.read(inputBuffer))>0) {
+                // char to string conversion
+                String readstring = String.copyValueOf(inputBuffer,0,charRead);
+                s +=readstring;
+            }
+            InputRead.close();
+            mainView.setText(s);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
